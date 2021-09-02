9c823b206dbe0b1712a9a594f9485f8b112c0131
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-09-02 11:26:11 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,23 @@
+
+
+public class App {
+
+	static String value = "Hi";
+	
+	public static void main(String[] args) {
+		
+		// KEINE Instanz --> statisch
+		Helper.showValue();
+		
+		// Helperklassen aus JAVA-System
+		System.out.println(Math.PI);
+		
+		// Helper --> Klasse, statisch
+		// helper --> Objekt/Instanz (dynamisch)
+		
+		System.out.println(value);
+
+		
+	}
+
+}
---
+++ Helper.java
@@ -0,0 +1,13 @@
+
+
+public class Helper {
+
+	private static String name = "Imran";
+	
+	public static void showValue() {
+		System.out.println(name);
+	}
+	
+	// KEIN this da KEINE Instanz!!!
+	
+}
