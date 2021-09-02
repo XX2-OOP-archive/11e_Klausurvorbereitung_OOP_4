83b4b48d072090afaabbebe4261fc90a7fa32587
Peter <schmidt.multimedia@gmail.com>
2021-09-02 11:50:11 +0200

    update

--- com/cc/java/Helper.java
+++ com/cc/java/Helper.java
@@ -3,7 +3,7 @@ package com.cc.java;
 
 public class Helper {
 
-	private static String name = "Imran";
+	private static String name = "Maurice";
 	
 	public static void showValue() {
 		System.out.println(name);
